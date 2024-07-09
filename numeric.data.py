306d1f2bf09a589260be34aa3d76676859b2ec11
Lawzyee <bi32bq@student.sunderland.ac.uk>
2024-07-09 16:23:05 +0100

    Add files via upload

---
+++ numeric.data.py
@@ -0,0 +1,27 @@
+print("Python has three numeric types: int, float, and complex")
+
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+print("Python has three numeric types: int, float, and complex")
+
+myValue=1.0
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+print("Python has three numeric types: int, float, and complex")
+
+myValue=1j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+print("Python has three numeric types: int, float, and complex")
+
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
